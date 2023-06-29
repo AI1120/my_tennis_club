a57c329a65a593036c2081d131d0ae7ab4635b82
alinwilson1120 <133474218+AI1120@users.noreply.github.com>
2023-06-29 05:28:20 -0400

    Initial commit

---
+++ members/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+# Create your views here.
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers' : mymembers
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember' : mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def testing(request):
+    # mydata = Member.objects.filter(firstname='Emil').values()
+    # mydata = Member.objects.values_list('firstname')
+    mydata = Member.objects.all()
+    template = loader.get_template('template.html')
+    context = {
+        'mymembers' : mydata,
+    }
+    return HttpResponse(template.render(context, request))
